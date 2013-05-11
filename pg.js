95db4e3f420129f199ecae4b72fe2c8b8a8f63c4
Mark Kollasch <markikollasch@gmail.com>
2013-05-11 10:12:07 -0700

    further interface sketching

--- pg.js
+++ pg.js
@@ -7,7 +7,7 @@
 var pg = pg || {};
 
 // enumeration of valid text unit states
-var pg.Status = {
+pg.Status = {
     BLANK: "Blank",
     UNFINISHED: "Unfinished",
     BAD: "Bad",
@@ -17,38 +17,32 @@ var pg.Status = {
 if (Object.freeze) { Object.freeze(pg.Status); }
 
 // text unit type definition
-// usage: new TextUnit(header, body, status (per pg.Status), tags (any number)
-// all arguments are optional
-var pg.TextUnit = function() {
-    if (arguments.length > 0) { // first argument - head
-        this.head = arguments[0];
-        if (arguments.length > 1) { // second argument - body
-            this.body = arguments[1];
-            if (arguments.length > 2) { // third argument - status
-                this.status = arguments[2];
-                if (arguments.length > 3) { // subsequent arguments - tags
-                    this.tags = arguments.slice(3);
-                }
-                else {
-                    this.tags = [];
-            }
-            else {
-                this.status = pg.Status.BLANK;
-            }
-        else {
-            this.body = "";
-        }
-    } else {
-        this.head = "";
-    }
+pg.TextUnit = function(head, body, status) {
+    this.head = head===undefined ? "" : head;
+    this.body = body===undefined ? "" : body;
+    this.status = status===undefined ? "" : pg.Status.BLANK;
 };
-pg.TextUnit.prototype.addTag = function(tag) {
-    if (tags.indexOf(tag) == -1)
-        tags.push(tag);
-};
-pg.TextUnit.prototype.removeTag = function(tag) {
-    tags.forEach(function(element, index, array) {
-        if (element === tag)
-            tags.splice(index, 1);
-    });
+
+pg.TextUnitDisplay = function(unit) {
+    this.unit = unit===undefined? new TextUnit() : unit;
+    
+    this.rootDiv = document.createElement("div");
+    this.rootDiv.className = "text-unit";
+    
+    this.headDiv = document.createElement("div");
+    this.headDiv.className = "unit-head";
+    this.headDiv.contentEditable = true;
+    this.headDiv.innerHTML = this.unit.head;
+    
+    this.bodyDiv = document.createElement("div");
+    this.bodyDiv.className = "unit-body";
+    this.bodyDiv.contentEditable = true;
+    this.bodyDiv.innerHTML = this.unit.body;
+    
+    //TODO: create the controls as well
+    
+    this.rootDiv.appendChild(this.headDiv);
+    this.rootDiv.appendChild(this.bodyDiv);
 }
+
+pg.AdderDisplay
