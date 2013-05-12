d7e2f2952aa712c84aa95c6118e1f590bad2d057
Mark Kollasch <markikollasch@gmail.com>
2013-05-12 11:45:59 -0700

    core UI functionality complete
    turns out this stuff counts as tabular data after all;
    for testing purposes, it now prepopulates the app with 10 empty fields
    they will be automatically colored as their status changes and
    everything (but there's not yet a way to change their status)

--- pg.js
+++ pg.js
@@ -8,11 +8,11 @@ var pg = pg || {};
 
 // enumeration of valid text unit states
 pg.Status = {
-    BLANK: "Blank",
-    UNFINISHED: "Unfinished",
-    BAD: "Bad",
-    PRETTY_GOOD: "Pretty Good",
-    GREAT: "Great"
+    BLANK: {value: 0, name: "Blank"},
+    UNFINISHED: {value: 1, name: "Unfinished"},
+    BAD: {value: 2, name: "Bad"},
+    OK: {value: 3, name: "Pretty Good"},
+    GREAT: {value: 4, name: "Great"}
 };
 if (Object.freeze) { Object.freeze(pg.Status); }
 
@@ -20,29 +20,76 @@ if (Object.freeze) { Object.freeze(pg.Status); }
 pg.TextUnit = function(head, body, status) {
     this.head = head===undefined ? "" : head;
     this.body = body===undefined ? "" : body;
-    this.status = status===undefined ? "" : pg.Status.BLANK;
+    this.status = status===undefined ? pg.Status.BLANK : status;
 };
 
-pg.TextUnitDisplay = function(unit) {
-    this.unit = unit===undefined? new TextUnit() : unit;
+// visually represents and manipulates a text unit
+pg.TextUnitDisplay = function(u) {
+    /*  <table class="text-unit" data-status="Whatever">
+     *      <tr class="unit-editor">
+     *          <td class="unit-head></td>
+     *          <td class="unit-body></td>
+     *      </tr>
+     *      <tfoot class="unit-controls" colspan="2">
+     *          <td class="unit-controls-inner" colspan="2"></td>
+     *      </tfoot>
+     *  </div>
+     */
+    var unit = u===undefined? new TextUnit() : u;
     
-    this.rootDiv = document.createElement("div");
+    this.rootDiv = document.createElement("table");
     this.rootDiv.className = "text-unit";
+    this.rootDiv.setAttribute("data-status", unit.status.value);
     
-    this.headDiv = document.createElement("div");
-    this.headDiv.className = "unit-head";
-    this.headDiv.contentEditable = true;
-    this.headDiv.innerHTML = this.unit.head;
+    this.editorDiv = document.createElement("tr");
+    this.editorDiv.className = "unit-editor";
     
-    this.bodyDiv = document.createElement("div");
-    this.bodyDiv.className = "unit-body";
-    this.bodyDiv.contentEditable = true;
-    this.bodyDiv.innerHTML = this.unit.body;
+    // head
+    var headDiv = document.createElement("td");
+    headDiv.className = "unit-head";
+    headDiv.contentEditable = true;
+    headDiv.innerHTML = unit.head;
+    // synchronize inner data with representation
+    headDiv.addEventListener("input", function() {
+        unit.head = headDiv.innerHTML; });
+    this.headDiv = headDiv;
     
-    //TODO: create the controls as well
+    // body
+    var bodyDiv = document.createElement("td");
+    bodyDiv.className = "unit-body";
+    bodyDiv.contentEditable = true;
+    bodyDiv.innerHTML = unit.body;
+    // synchronize inner data with representation
+    bodyDiv.addEventListener("input", function() {
+        unit.body = bodyDiv.innerHTML; });
+    this.bodyDiv = bodyDiv;
     
-    this.rootDiv.appendChild(this.headDiv);
-    this.rootDiv.appendChild(this.bodyDiv);
+    // controls
+    var ctrlDiv = document.createElement("tfoot");
+    ctrlDiv.className = "unit-controls";
+    var ctrlDivInner = document.createElement("td");
+    ctrlDivInner.className = "unit-controls-inner";
+    ctrlDivInner.colSpan = "2";
+    ctrlDivInner.innerHTML = "CONTROLS PLACEHOLDER";
+    ctrlDiv.appendChild(ctrlDivInner);
+    
+    this.rootDiv.addEventListener("mouseover", function() {
+        ctrlDiv.style.visibility = "visible";});
+    this.rootDiv.addEventListener("mouseleave", function() {
+        ctrlDiv.style.visibility = "collapse";});
+    
+    this.ctrlDivInner = ctrlDivInner;
+    this.ctrlDiv = ctrlDiv;
+    
+    this.unit = unit;
+    this.editorDiv.appendChild(this.headDiv);
+    this.editorDiv.appendChild(this.bodyDiv);
+    this.rootDiv.appendChild(this.editorDiv);
+    this.rootDiv.appendChild(this.ctrlDiv);
 }
 
-pg.AdderDisplay
+pg.workspace = document.getElementById("workspace");
+
+for (var i=0; i<10; i++) {
+    pg.workspace.appendChild(new pg.TextUnitDisplay(new pg.TextUnit()).rootDiv);
+}
